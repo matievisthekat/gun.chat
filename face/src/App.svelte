41e466788a0ffbea022fdf8a3d1264c45510c3cf
matievisthekat <matievisthekat@gmail.com>
2021-12-30 23:10:59 +0200

    add logic

--- face/src/App.svelte
+++ face/src/App.svelte
@@ -1,16 +1,41 @@
 <script lang="ts">
   import './assets/styles/App.scss';
-  import GUN from 'gun/gun';
+  import GUN from 'gun';
+  import 'gun/sea';
 
   const gun = GUN(['http://localhost:8765/gun']);
+
+  const submit = () => {
+    if (!user) return (err = 'No username');
+
+    gun.user().create(user, pass, (ack) => {
+      if ('err' in ack) {
+        err = ack.err;
+      } else {
+        //!todo: something i guess
+      }
+    });
+  };
+
+  let user: string;
+  let pass: string;
+  let err: string;
 </script>
 
 <main>
-  <form autocomplete="off">
+  <form autocomplete="off" on:submit|preventDefault={submit}>
     <label for="username">username</label>
-    <input type="text" name="username" id="username" />
+    <input type="text" name="username" id="username" bind:value={user} />
 
     <label for="passphrase">passphrase</label>
-    <input type="password" name="passphrase" id="passphrase" />
+    <input
+      type="password"
+      name="passphrase"
+      id="passphrase"
+      bind:value={pass} />
+
+    <button type="submit">create</button>
+
+    <span id="err"><strong>Error:</strong> {err ?? 'none'}</span>
   </form>
 </main>
