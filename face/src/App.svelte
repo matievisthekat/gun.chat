a1494ea9dcd69fe5abdadca073da2b19c760183e
matievisthekat <matievisthekat@gmail.com>
2021-12-30 22:47:34 +0200

    init

---
+++ face/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import './assets/styles/App.scss';
+  import GUN from 'gun/gun';
+
+  const gun = GUN(['http://localhost:8765/gun']);
+</script>
+
+<main>
+  <form autocomplete="off">
+    <label for="username">username</label>
+    <input type="text" name="username" id="username" />
+
+    <label for="passphrase">passphrase</label>
+    <input type="password" name="passphrase" id="passphrase" />
+  </form>
+</main>
