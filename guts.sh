31f00a08934a3b9087d0c0feecef4ee67243ac1b
matievisthekat <matievisthekat@gmail.com>
2022-01-08 10:12:20 +0200

    auto generate keypair if none exists

--- guts.sh
+++ guts.sh
@@ -1,1 +1,13 @@
+if [ ! -f ./server.crt ] || [ ! -f ./server.pem ]; then
+  openssl req -newkey rsa:4096 \
+    -x509 \
+    -sha256 \
+    -days 365 \
+    -nodes \
+    -out server.crt \
+    -keyout server.pem \
+    -subj "/C=ZA/CN=Matthew Stead" \
+    -batch
+fi
+
 docker run -p 8765:8765 gundb/gun
