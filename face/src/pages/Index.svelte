b904b9b97b5060d0cb67d8b578a095bdca7296a2
matievisthekat <matievisthekat@gmail.com>
2022-01-09 20:26:14 +0200

    navigate to /${ack.sea.pub} on authentication

--- face/src/pages/Index.svelte
+++ face/src/pages/Index.svelte
@@ -2,10 +2,13 @@
   import type {Ack} from '../types';
   import '../assets/styles/pages/Index.scss';
   import AuthForm from '../components/AuthForm.svelte';
+  import {navigate} from 'svelte-routing';
   import {loadGun} from '../util';
 
   const gun = loadGun();
-  const onAuthenticated = (ack: Ack) => {};
+  const onAuthenticated = (ack: Ack) => {
+    navigate(`/${ack.sea.pub}`);
+  };
 </script>
 
 <main>
