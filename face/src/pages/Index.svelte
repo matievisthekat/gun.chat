d4b2b60aef44f028ddd143883503c1db185131b6
matievisthekat <matievisthekat@gmail.com>
2022-01-09 19:44:44 +0200

    update onAuthenticated to use Ack interface

--- face/src/pages/Index.svelte
+++ face/src/pages/Index.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  import type {User} from '../types';
+  import type {Ack} from '../types';
   import '../assets/styles/pages/Index.scss';
   import AuthForm from '../components/AuthForm.svelte';
   import {loadGun} from '../util';
 
   const gun = loadGun();
-  const onAuthenticated = (usr: User) => {};
+  const onAuthenticated = (ack: Ack) => {};
 </script>
 
 <main>
