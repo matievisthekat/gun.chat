563ca9b51503d720fa6489422988725a2698166e
matievisthekat <matievisthekat@gmail.com>
2022-01-09 19:44:04 +0200

    add user page

--- face/src/App.svelte
+++ face/src/App.svelte
@@ -2,10 +2,12 @@
 <script>
   import {Router, Route} from 'svelte-routing';
   import Index from './pages/Index.svelte';
+  import User from './pages/User.svelte';
 
   export let url = '';
 </script>
 
 <Router {url}>
   <Route path="/"><Index /></Route>
+  <Route path="/@:pub" let:params><User /></Route>
 </Router>
---
+++ face/src/pages/User.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  // import {loadGun} from '../util';
+
+  // const gun = loadGun();
+
+  export let pub = '';
+</script>
+
+<h1>{pub}</h1>
