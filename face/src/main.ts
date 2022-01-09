7170f483b78a667056eeec238a69cb3c62b5c9ef
matievisthekat <matievisthekat@gmail.com>
2022-01-09 19:20:54 +0200

    set hydrate to true

--- face/src/main.ts
+++ face/src/main.ts
@@ -1,6 +1,6 @@
 import './assets/styles/global.scss';
 import App from './pages/Index.svelte';
 
-const app = new App({target: document.getElementById('app')});
+const app = new App({target: document.getElementById('app'), hydrate: true});
 
 export default app;
